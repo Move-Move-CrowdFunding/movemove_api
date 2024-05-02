a6bd71c761330d331a6dfcbb1dc68bb164e208d0
Hank <tw1720@gmail.com>
2024-05-02 15:20:49 +0800

    Model schema 建立: Sponsor, Track, Check

--- models/Check.ts
+++ models/Check.ts
@@ -1,6 +1,29 @@
 import mongoose from 'mongoose'
 
-const schema = {}
+const schema = {
+  projectId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Project',
+    required: [true, '請輸入提案id']
+  },
+  content: {
+    type: String
+  },
+  status: {
+    type: Number,
+    required: [true, '請輸入審核狀態'],
+    enum: {
+      values: [0, 1, -1],
+      message: '審核狀態碼錯誤'
+    }
+  },
+  createTime: {
+    type: Number
+  },
+  updateTime: {
+    type: Number
+  }
+}
 
 const option = {
   versionKey: false
--- models/Track.ts
+++ models/Track.ts
@@ -1,6 +1,23 @@
 import mongoose from 'mongoose'
 
-const schema = {}
+const schema = {
+  userId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, '請輸入用戶id']
+  },
+  projectId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Project',
+    required: [true, '請輸入提案id']
+  },
+  createTime: {
+    type: Number
+  },
+  updateTime: {
+    type: Number
+  }
+}
 
 const option = {
   versionKey: false
