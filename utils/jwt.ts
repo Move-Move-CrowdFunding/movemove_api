c580a5d8cb5568c81477806eac3d06defc368f81
lingling <zzxcv741@hotmail.com>
2024-05-14 23:17:57 +0800

    feat: forget password done

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -10,3 +10,26 @@ export const verifyJWTtoken = (token: string) => {
   const SecretKey = process.env.JWT_SECRET_KEY || ''
   return jwt.verify(token, SecretKey)
 }
+
+export const generateEmailToken = () => {
+  const code = generateRandomCode()
+
+  const token = jwt.sign({ code }, process.env.JWT_SECRET_KEY || '', {
+    expiresIn: 3600
+  })
+
+  return { code, token }
+}
+
+const generateRandomCode = () => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  let code = ''
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length)
+    code += characters.charAt(randomIndex)
+  }
+
+  return code
+}
