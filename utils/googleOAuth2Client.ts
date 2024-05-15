6c28bcff624e437164ff3fdb6097dcbfc0a3eb2c
Hank <tw1720@gmail.com>
2024-05-15 13:23:21 +0800

    google auth import 調整

--- utils/googleOAuth2Client.ts
+++ utils/googleOAuth2Client.ts
@@ -1,6 +1,6 @@
-import { google } from 'googleapis'
+import { auth } from 'googleapis/build/src/apis/oauth2'
 
-const googleOAuth2Client = new google.auth.OAuth2(
+const googleOAuth2Client = new auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
   process.env.GMAIL_REDIRECT_URL
