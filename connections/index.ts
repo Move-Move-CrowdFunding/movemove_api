1e19949fccdb79d80c0b59b4e5d4eb52b61c14d8
yichienlu <yichien.lu89@gmail.com>
2024-05-30 11:33:45 +0800

    feat: achievedMoney

--- connections/index.ts
+++ connections/index.ts
@@ -9,8 +9,10 @@ const connectDB = () => {
     return
   }
 
-  const DB = String(process.env.MONGODB_URL).replace('<password>', String(process.env.MONGODB_PASSWORD))
-  mongoose.connect(DB).then(() => console.log(`${process.env.MONGODB_URL} 連接成功`))
+  // const DB = String(process.env.MONGODB_URL).replace('<password>', String(process.env.MONGODB_PASSWORD))
+  // mongoose.connect(DB).then(() => console.log(`${process.env.MONGODB_URL} 連接成功`))
+  const DB1 = "mongodb://localhost:27017/movemove"
+  mongoose.connect(DB1).then(() => console.log('連接成功'))
 }
 
 export default connectDB
