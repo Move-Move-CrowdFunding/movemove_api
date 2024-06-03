9b8cd5bb2984e9d29b331724ff49af0462634065
Hank <tw1720@gmail.com>
2024-06-03 21:15:34 +0800

    修改了後台提案列表與提案內容API

--- routes/admin.ts
+++ routes/admin.ts
@@ -35,7 +35,7 @@ router.get('/projects', authMiddleware, checkAdminAuth, async (req, res) => {
       type: 'boolean',
       default: 'true'
     }
-    * #swagger.parameters['status'] = {
+    * #swagger.parameters['state'] = {
       in: 'query',
       description: '提案狀態 （N = 0:[預設]送審 1:核准 -1:否准 2:已結束 3:全部）',
       type: 'number',
@@ -75,7 +75,11 @@ router.get('/projects', authMiddleware, checkAdminAuth, async (req, res) => {
           "createTime": 1716704528,
           "updateTime": 1716704528,
           "nickName": "",
-          "status": 0
+        "reviewLog": [{
+            "_id": "6646ac449c84d6afa1853002",
+            "content": "請核准提案",
+            "status": 0,
+            "timestamp": 1716007650
         }],
         "pagination": {
             "pageNo": 1,
@@ -83,7 +87,7 @@ router.get('/projects', authMiddleware, checkAdminAuth, async (req, res) => {
             "totalPage": 1,
             "count": 1,
             "sortDesc": true,
-            "status": 0,
+            "state": 0,
             "search": "關鍵字搜尋"
           }
       }
@@ -95,7 +99,7 @@ router.get('/projects', authMiddleware, checkAdminAuth, async (req, res) => {
   try {
     // 請求參數檢查
     const errorMsg = []
-    const { pageNo = 1, pageSize = 10, sortDesc = 'false', status = 0, search = '' } = req.query
+    const { pageNo = 1, pageSize = 10, sortDesc = 'false', state = 0, search = '' } = req.query
     let sort = 1
     // 當前頁數
     if (!Number(pageNo) || Number(pageNo) < 1) {
@@ -115,7 +119,7 @@ router.get('/projects', authMiddleware, checkAdminAuth, async (req, res) => {
     }
 
     // 提案狀態 （N = 0:[預設]送審 1:核准 -1:否准 2:已結束 3:全部）
-    if (![0, 1, -1, 2, 3].includes(Number(status))) {
+    if (![0, 1, -1, 2, 3].includes(Number(state))) {
       errorMsg.push('提案狀態錯誤')
     }
 
@@ -125,20 +129,9 @@ router.get('/projects', authMiddleware, checkAdminAuth, async (req, res) => {
       let totalProjects = 0
 
       // 提案狀態查詢邏輯
-      let statusFilter = {}
-      switch (Number(status)) {
-        case 0:
-          statusFilter = { 'reviewLog.status': 0 }
-          break
-        case 1:
-          statusFilter = { 'reviewLog.status': 1 }
-          break
-        case -1:
-          statusFilter = { 'reviewLog.status': -1 }
-          break
-        case 2:
-          statusFilter = { endDate: { $lt: Date.now() / 1000 } }
-          break
+      let stateFilter = {}
+      if ([0, 1, -1, 2].includes(Number(state))) {
+        stateFilter = Number(state) === 2 ? { endDate: { $lt: Date.now() / 1000 } } : { 'state.status': state }
       }
 
       filter = { title: { $regex: search } }
@@ -164,16 +157,11 @@ router.get('/projects', authMiddleware, checkAdminAuth, async (req, res) => {
               { $sort: { updateTime: -1 } },
               { $limit: 1 }
             ],
-            as: 'reviewLog'
+            as: 'state'
           }
         },
-        { $unwind: '$reviewLog' },
-        { $match: statusFilter },
-        {
-          $addFields: {
-            status: '$reviewLog.status'
-          }
-        }
+        { $unwind: '$state' },
+        { $match: stateFilter }
       ]).then((results) => {
         totalProjects = results.length
       })
@@ -216,31 +204,38 @@ router.get('/projects', authMiddleware, checkAdminAuth, async (req, res) => {
                   }
                 }
               },
-              { $sort: { updateTime: -1 } },
-              { $limit: 1 } // 只查最新的審核紀錄
+              { $sort: { updateTime: 1 } }
             ],
             as: 'reviewLog'
           }
         },
-        { $unwind: '$reviewLog' },
-        { $match: statusFilter },
         {
           $addFields: {
-            status: {
-              $switch: {
-                branches: [
-                  { case: { $lt: ['$endDate', Date.now() / 1000] }, then: 2 } // 代號2為已結束的提案
-                ],
-                default: '$reviewLog.status'
+            reviewLog: {
+              $map: {
+                input: '$reviewLog',
+                as: 'check',
+                in: {
+                  $mergeObjects: ['$$check', { timestamp: '$$check.updateTime' }]
+                }
               }
             }
           }
         },
-        { $unwind: '$status' },
+        {
+          $addFields: {
+            state: { $arrayElemAt: ['$reviewLog', -1] }
+          }
+        },
+        { $unwind: '$state' },
+        { $match: stateFilter },
         {
           $project: {
             userId: 0,
-            reviewLog: 0
+            state: 0,
+            'reviewLog.projectId': 0,
+            'reviewLog.createTime': 0,
+            'reviewLog.updateTime': 0
           }
         },
         { $sort: { startDate: sort as any } },
@@ -258,7 +253,7 @@ router.get('/projects', authMiddleware, checkAdminAuth, async (req, res) => {
           totalPage,
           count: totalProjects,
           sortDesc: String(sortDesc),
-          status,
+          state: Number(state),
           search: String(search)
         }
       })
@@ -290,37 +285,41 @@ router.get('/projects/:projectId', authMiddleware, checkAdminAuth, async (req, r
       "status": "success",
       "message": "取得提案資料成功",
       "results": {
-        "_id": "66401d4618d9a03d581946fc",
-        "introduce": "大家好，我們是廣福國小六年級資優班的學生，我們希望能透過自己的能力及創意籌得資優班畢旅的資金。 老師教導我們要成為懂得幫助他人的人，所以在每次籌辦活動後我們都會做一件幫助他人的事情！",
-        "teamName": "愛在西元前",
-        "email": "elsa@gamil.com",
-        "phone": "0955123123",
-        "title": "六優畢旅自籌計畫—【一趟讓國小資優生實踐自主公益計畫的畢業旅行】",
-        "categoryKey": 1,
-        "targetMoney": 100000,
-        "startDate": 1714534369,
-        "endDate": 1715439897,
-        "describe": "一輩子只有一次的國小資優生畢業旅行",
-        "coverUrl": "https://picsum.photos/id/65/200/300",
-        "content": "<p>在資優班情意課課程時，老師剛好上到我們如何「同理他人」，我們想到普通班會去一次畢旅，爸爸媽媽就要負擔4張小朋友左右的費用，如果資優班的畢旅可以靠我們自己的能力籌得經費，那不但可以替爸爸媽媽省錢，也能在過程中學習如何籌畫好一個計劃。<br><br>於是計畫就這麼開始了！<br><br>在大家有共識決定自籌畢旅，在腦力激盪階段，我們把從四年級到現在在資優班學習到的概念拿來推敲一番，四年級情意課學習「事情的一體兩面」、五年級特色課程從「能源議題」出發，結合SDGs聯合國永續發展目標我們學習到能源從哪裡來、省電的重要、什麼是碳排放以及永續的重要性，並執行了「檢視自己一週食衣住行育樂碳排放量」的活動，從中我們學會了關注生活及環保觀念。</p>",
-        "videoUrl": "https://youtu.be/Iy69ifIf7jc?si=rX3_ZAdEhvgsUbu5",
-        "relatedUrl": "www.google.com",
-        "feedbackItem": "學生書寫的電子感謝函",
-        "feedbackUrl": "",
-        "feedbackMoney": 5000,
-        "feedbackDate": 1718118294,
+        "_id": "66403c34b00d1fe281742a62",
+        "introduce": "專業金援團隊，弱勢族群救星，幫助許多需要協助的家庭。",
+        "teamName": "弱勢救星",
+        "email": "nomail@mail.com",
+        "phone": "0938938438",
+        "title": "測試修改送審",
+        "categoryKey": 2,
+        "targetMoney": 30000,
+        "startDate": 1720713600,
+        "endDate": 1721577600,
+        "describe": "一場無情的大火吞噬了整個社區，請幫助無家可歸的民眾。",
+        "coverUrl": "https://fakeimg.pl/300/",
+        "content": "<p>test</p>",
+        "videoUrl": "",
+        "relatedUrl": "",
+        "feedbackItem": "限量精美小熊維尼",
+        "feedbackUrl": "https://fakeimg.pl/300/",
+        "feedbackMoney": 100,
+        "feedbackDate": 1721577600,
+        "createTime": 1715485748,
+        "updateTime": 1716103058,
         "reviewLog": [
           {
-            "_id": "663e4c59d8b15d4452a72a63",
-            "content": "待審核",
+            "_id": "6646ac449c84d6afa1853002",
+            "content": "請核准提案",
             "status": 0,
-            "timestamp": 1715358809
+            "timestamp": 1716007650
+          },
+          {
+            "_id": "6646ac449c84d6afa1853003",
+            "content": "管理員已核准提案",
+            "status": 1,
+            "timestamp": 1716007669
           }
-        ],
-        "state": {
-          "state": 2,
-          "content": "已結束"
-        }
+        ]
       }
     }
   }
@@ -351,22 +350,6 @@ router.get('/projects/:projectId', authMiddleware, checkAdminAuth, async (req, r
       {
         $match: { _id: new Types.ObjectId(projectId) }
       },
-      // {
-      //   $lookup: {
-      //     from: 'users',
-      //     let: { userId: '$userId' },
-      //     pipeline: [
-      //       {
-      //         $match: {
-      //           $expr: { $eq: ['$_id', { $toObjectId: '$$userId' }] }
-      //         }
-      //       },
-      //       { $project: { password: 0 } }
-      //     ],
-      //     as: 'userId'
-      //   }
-      // },
-      // { $unwind: '$userId' },
       {
         $lookup: {
           from: 'checks',
@@ -398,25 +381,8 @@ router.get('/projects/:projectId', authMiddleware, checkAdminAuth, async (req, r
         }
       },
       {
-        $addFields: {
-          latestCheck: { $arrayElemAt: ['$reviewLog', -1] }
-        }
-      },
-      {
-        $addFields: {
-          state: {
-            $switch: {
-              branches: [{ case: { $lt: ['$endDate', Date.now() / 1000] }, then: { state: 2, content: '已結束' } }],
-              default: { state: '$latestCheck.status', content: '$latestCheck.content' }
-            }
-          }
-        }
-      },
-      { $unwind: '$state' },
-      {
         $project: {
           userId: 0,
-          latestCheck: 0,
           'reviewLog.projectId': 0,
           'reviewLog.createTime': 0,
           'reviewLog.updateTime': 0
